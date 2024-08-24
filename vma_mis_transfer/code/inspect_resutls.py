118a5003864e2b92f224efa4efb32c4630c82590
Matthew Crossley <mq20185996@Matthews-Air.socal.rr.com>
2024-08-24 13:02:30 -0700

    gabria

--- vma_mis_transfer/code/inspect_resutls.py
+++ vma_mis_transfer/code/inspect_resutls.py
@@ -27,6 +27,7 @@ def interpolate_movements(d):
     dd = pd.DataFrame({"relsamp": relsamp, "t": tt, "x": xx, "y": yy, "v": vv})
     dd["condition"] = d["condition"].unique()[0]
     dd["subject"] = d["subject"].unique()[0]
+    dd["session"] = d["session"].unique()[0]
     dd["trial"] = d["trial"].unique()[0]
     dd["target_angle"] = d["target_angle"].unique()[0]
     dd["phase"] = d["phase"].unique()[0]
@@ -75,141 +76,153 @@ d_rec = []
 dd_rec = []
 ddd_rec = []
 
-plot_sub_trajectories = True
+# NOTE: useful when visually inspecting the data at the
+# subject level
+plot_sub_trajectories = False
 
-for s in range(1, 2):
+sub_nums = [1, 3, 4, 5, 6, 7, 8, 9, 10]
 
-    f_trl = "sub_{}_data.csv".format(s)
-    f_mv = "sub_{}_data_move.csv".format(s)
+for s in sub_nums:
 
-    try:
-        d_trl = pd.read_csv(os.path.join(dir_data, f_trl))
-        d_mv = pd.read_csv(os.path.join(dir_data, f_mv))
+    f_trl_1 = "sub_{}_data.csv".format(s)
+    f_mv_1 = "sub_{}_data_move.csv".format(s)
 
-        n_trial_trl = d_trl["trial"].nunique()
-        n_trial_mv = d_mv["trial"].nunique()
+    f_trl_2 = "sub_{}{}_data.csv".format(s, s)
+    f_mv_2 = "sub_{}{}_data_move.csv".format(s, s)
 
-    except FileNotFoundError:
-        n_trial_trl = 0
-        n_trial_mv = 0
+    d_trl_1 = pd.read_csv(os.path.join(dir_data, f_trl_1))
+    d_mv_1 = pd.read_csv(os.path.join(dir_data, f_mv_1))
 
-    if (n_trial_trl == 407) and (n_trial_mv == 407):
+    d_trl_2 = pd.read_csv(os.path.join(dir_data, f_trl_2))
+    d_mv_2 = pd.read_csv(os.path.join(dir_data, f_mv_2))
 
-        d_trl = d_trl.sort_values(["condition", "subject", "trial"])
-        d_mv = d_mv.sort_values(["condition", "subject", "t", "trial"])
-        d_hold = d_mv[d_mv["state"].isin(["state_holding"])]
-        x_start = d_hold.x.mean()
-        y_start = d_hold.y.mean()
+    d_trl_1["session"] = 1
+    d_mv_1["session"] = 1
 
-        d_mv = d_mv[d_mv["state"].isin(["state_moving"])]
+    d_trl_2["session"] = 2
+    d_mv_2["session"] = 2
 
-        phase = np.empty(d_trl.shape[0], dtype="object")
+    d_trl_2["subject"] = d_trl_1["subject"].unique()[0]
+    d_mv_2["subject"] = d_mv_1["subject"].unique()[0]
 
-        # The experiment began with a familiarization phase of 33
-        # reach trials (3 trials per target in pseudorandom order)
-        # with continuous veridical visual feedback provided
-        # throughout the reach.
-        phase[:33] = "familiarisation"
+    d_trl = pd.concat([d_trl_1, d_trl_2])
+    d_mv = pd.concat([d_mv_1, d_mv_2])
 
-        # The baseline phase consisted of 198 reach trials across
-        # all 11 target directions (18 trials per target). On each
-        # trial, the location of the target was randomized across
-        # participants. For 2/3 of the reaches (132 trials),
-        # continuous veridical cursor feedback was provided
-        # throughout the trial. For the remaining 1/3 (66 trials),
-        # visual feedback was completely withheld (i.e., no feedback
-        # was given during the reach and no feedback was given at
-        # the end of the reach about reach accuracy).
-        phase[33:231] = "baseline"
+    d_trl = d_trl.sort_values(["condition", "subject", "trial"])
+    d_mv = d_mv.sort_values(["condition", "subject", "t", "trial"])
+    d_hold = d_mv[d_mv["state"].isin(["state_holding"])]
+    x_start = d_hold.x.mean()
+    y_start = d_hold.y.mean()
 
-        # The adaptation phase consisted of 110 reaches toward a
-        # single target positioned at 0° in the frontal plane
-        # (straight ahead; see Fig. 1b). During this phase, endpoint
-        # feedback was rotated about the starting position by 30°
-        # (CW or CCW; counterbalanced between participants).
-        phase[231:341] = "adaptation"
+    d_mv = d_mv[d_mv["state"].isin(["state_moving"])]
 
-        # The generalization phase consisted of 66 reaches to 1 of
-        # 11 target directions (10 untrained directions) presented
-        # in pseudorandom order without visual feedback.
-        phase[341:] = "generalization"
+    phase = np.empty(d_trl.shape[0], dtype="object")
 
-        d_trl["phase"] = phase
+    # The experiment began with a familiarization phase of 33
+    # reach trials (3 trials per target in pseudorandom order)
+    # with continuous veridical visual feedback provided
+    # throughout the reach.
+    phase[:33] = "familiarisation"
 
-        d_trl["su"] = d_trl["su"].astype("category")
+    # The baseline phase consisted of 198 reach trials across
+    # all 11 target directions (18 trials per target). On each
+    # trial, the location of the target was randomized across
+    # participants. For 2/3 of the reaches (132 trials),
+    # continuous veridical cursor feedback was provided
+    # throughout the trial. For the remaining 1/3 (66 trials),
+    # visual feedback was completely withheld (i.e., no feedback
+    # was given during the reach and no feedback was given at
+    # the end of the reach about reach accuracy).
+    phase[33:231] = "baseline"
 
-        d_trl["ep"] = (d_trl["ep"] * 180 / np.pi) + 90
-        d_trl["rotation"] = d_trl["rotation"] * 180 / np.pi
+    # The adaptation phase consisted of 110 reaches toward a
+    # single target positioned at 0° in the frontal plane
+    # (straight ahead; see Fig. 1b). During this phase, endpoint
+    # feedback was rotated about the starting position by 30°
+    # (CW or CCW; counterbalanced between participants).
+    phase[231:341] = "adaptation"
 
-        d = pd.merge(d_mv,
-                     d_trl,
-                     how="outer",
-                     on=["condition", "subject", "trial"])
+    # The generalization phase consisted of 66 reaches to 1 of
+    # 11 target directions (10 untrained directions) presented
+    # in pseudorandom order without visual feedback.
+    phase[341:] = "generalization"
 
-        d = d.groupby(["condition", "subject", "trial"],
-                      group_keys=False).apply(compute_kinematics)
+    d_trl["phase"] = phase
 
-        dd = d.groupby(
-            ["condition", "subject", "phase", "target_angle", "trial"],
-            group_keys=False).apply(interpolate_movements)
+    d_trl["su"] = d_trl["su"].astype("category")
 
-        ddd = (dd.groupby(
-            ["condition", "subject", "phase", "target_angle",
-             "relsamp"])[["t", "x", "y", "v"]].mean().reset_index())
+    d_trl["ep"] = (d_trl["ep"] * 180 / np.pi) + 90
+    d_trl["rotation"] = d_trl["rotation"] * 180 / np.pi
 
-        if plot_sub_trajectories:
+    d = pd.merge(d_mv,
+                 d_trl,
+                 how="outer",
+                 on=["condition", "subject", "session", "trial"])
 
-            lbx, ubx = -200, 200
-            lby, uby = -200, 200
+    d = d.groupby(["condition", "subject", "session", "trial"],
+                  group_keys=False).apply(compute_kinematics)
 
-            fig, ax = plt.subplots(2, 2, squeeze=False)
-            ax = ax.flatten()
-            for i, p in enumerate(d.phase.unique()):
-                sns.scatterplot(data=d[d["phase"] == p],
-                                x="x",
-                                y="y",
-                                hue="trial",
-                                legend=False,
-                                ax=ax[i])
-                ax[i].set_xlim(lbx, ubx)
-                ax[i].set_ylim(lby, uby)
-                ax[i].set_title("Phase: " + p)
-                plt.tight_layout()
-            plt.show()
+    dd = d.groupby(
+        ["condition", "subject", "session", "phase", "target_angle", "trial"],
+        group_keys=False).apply(interpolate_movements)
 
-            fig, ax = plt.subplots(2, 2, squeeze=False)
-            ax = ax.flatten()
-            for i, p in enumerate(dd.phase.unique()):
-                sns.scatterplot(data=dd[dd["phase"] == p],
-                                x="x",
-                                y="y",
-                                hue="trial",
-                                legend=False,
-                                ax=ax[i])
-                ax[i].set_xlim(lbx, ubx)
-                ax[i].set_ylim(lby, uby)
-                ax[i].set_title("Phase: " + p)
-                plt.tight_layout()
-            plt.show()
+    ddd = (dd.groupby(
+        ["condition", "subject", "session", "phase", "target_angle",
+         "relsamp"])[["t", "x", "y", "v"]].mean().reset_index())
 
-            fig, ax = plt.subplots(2, 2, squeeze=False)
-            ax = ax.flatten()
-            for i, p in enumerate(ddd.phase.unique()):
-                sns.scatterplot(data=ddd[ddd["phase"] == p],
-                                x="x",
-                                y="y",
-                                hue="target_angle",
-                                legend=True,
-                                ax=ax[i])
-                ax[i].set_xlim(lbx, ubx)
-                ax[i].set_ylim(lby, uby)
-                ax[i].set_title("Phase: " + p)
-                plt.tight_layout()
-            plt.show()
+    if plot_sub_trajectories:
 
-        d_rec.append(d)
-        dd_rec.append(dd)
-        ddd_rec.append(ddd)
+        lbx, ubx = -200, 200
+        lby, uby = -200, 200
+
+        fig, ax = plt.subplots(2, 2, squeeze=False)
+        ax = ax.flatten()
+        for i, p in enumerate(d.phase.unique()):
+            sns.scatterplot(data=d[d["phase"] == p],
+                            x="x",
+                            y="y",
+                            hue="trial",
+                            legend=False,
+                            ax=ax[i])
+            ax[i].set_xlim(lbx, ubx)
+            ax[i].set_ylim(lby, uby)
+            ax[i].set_title("Phase: " + p)
+            plt.tight_layout()
+        plt.show()
+
+        fig, ax = plt.subplots(2, 2, squeeze=False)
+        ax = ax.flatten()
+        for i, p in enumerate(dd.phase.unique()):
+            sns.scatterplot(data=dd[dd["phase"] == p],
+                            x="x",
+                            y="y",
+                            hue="trial",
+                            legend=False,
+                            ax=ax[i])
+            ax[i].set_xlim(lbx, ubx)
+            ax[i].set_ylim(lby, uby)
+            ax[i].set_title("Phase: " + p)
+            plt.tight_layout()
+        plt.show()
+
+        fig, ax = plt.subplots(2, 2, squeeze=False)
+        ax = ax.flatten()
+        for i, p in enumerate(ddd.phase.unique()):
+            sns.scatterplot(data=ddd[ddd["phase"] == p],
+                            x="x",
+                            y="y",
+                            hue="target_angle",
+                            legend=True,
+                            ax=ax[i])
+            ax[i].set_xlim(lbx, ubx)
+            ax[i].set_ylim(lby, uby)
+            ax[i].set_title("Phase: " + p)
+            plt.tight_layout()
+        plt.show()
+
+    d_rec.append(d)
+    dd_rec.append(dd)
+    ddd_rec.append(ddd)
 
 d = pd.concat(d_rec)
 dd = pd.concat(dd_rec)
@@ -221,14 +234,10 @@ ddd = ddd.reset_index(drop=True)
 
 d.groupby(["condition"])["subject"].unique()
 
-d = d[~d["subject"].isin(exc_subs)]
-dd = dd[~dd["subject"].isin(exc_subs)]
-ddd = ddd[~ddd["subject"].isin(exc_subs)]
-
-dp = d.groupby(["condition", "phase", "trial", "target_angle", "rotation"],
+dp = d.groupby(["condition", "session", "phase", "trial", "target_angle", "rotation"],
                observed=True)[["imv", "emv"]].mean().reset_index()
 
-dp.sort_values(["condition", "trial"], inplace=True)
+dp.sort_values(["condition", "session", "trial"], inplace=True)
 
 dp.loc[dp["imv"] > 160, "imv"] -= 360
 
@@ -236,16 +245,36 @@ dp["imv"] = dp["imv"] - dp["target_angle"]
 
 dp["target_angle"] = dp["target_angle"].astype("category")
 
-fig, ax = plt.subplots(1, 2, squeeze=False)
+fig, ax = plt.subplots(2, 2, squeeze=False)
 sns.scatterplot(
-    data=dp,
+    data=dp[dp["session"] == 1],
     x="trial",
     y="imv",
     hue="target_angle",
     markers=True,
     ax=ax[0, 0],
 )
-ax[0, 0].plot(dp.trial, dp.rotation * 2, color="black", linestyle="-")
-sns.barplot(data=dp[dp["phase"] == "generalization"], x="target_angle", y="imv", ax=ax[0, 1])
+sns.scatterplot(
+    data=dp[dp["session"] == 2],
+    x="trial",
+    y="imv",
+    hue="target_angle",
+    markers=True,
+    ax=ax[1, 0],
+)
+ax[0, 0].plot(dp[dp["session"]==1].trial, dp[dp["session"]==1].rotation * 2, color="black", linestyle="-")
+ax[1, 0].plot(dp[dp["session"]==2].trial, dp[dp["session"]==2].rotation * 2, color="black", linestyle="-")
+sns.barplot(data=dp[(dp["phase"] == "generalization") & (dp["session"] == 1)],
+            x="target_angle",
+            y="imv",
+            ax=ax[0, 1])
+sns.barplot(data=dp[(dp["phase"] == "generalization") & (dp["session"] == 2)],
+            x="target_angle",
+            y="imv",
+            ax=ax[1, 1])
+ax[0, 0].set_ylim(-100, 100)
+ax[1, 0].set_ylim(-100, 100)
+ax[0, 0].set_title("Session 1")
+ax[1, 0].set_title("Session 2")
 plt.tight_layout()
 plt.show()
